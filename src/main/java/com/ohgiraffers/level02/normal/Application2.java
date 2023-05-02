b3544e5f1f3e32d637e46bb393b40076f9ecc497
AzureAD\양한진 <gkswls3203@naver.com>
2023-05-02 15:16:22 +0900

    [normal] [문제 '2'] 완료 실수 형태로 저장하기 위해 double, 정수형태로 출력하기 위해서 int 사용

--- src/main/java/com/ohgiraffers/level02/normal/Application2.java
+++ src/main/java/com/ohgiraffers/level02/normal/Application2.java
@@ -11,7 +11,12 @@ public class Application2 {
 		 * 총점 : 201
 		 * 평균 : 67
 		 *  */
-		
+		double num1 = 80.5;
+		double num2 = 50.6;
+		double num3 = 70.8;
+
+		System.out.println("총점 : " + (int)(num1+num2+num3));
+		System.out.println("평균 : " + (int)(num1+num2+num3) / 3);
 	}
 
 }
