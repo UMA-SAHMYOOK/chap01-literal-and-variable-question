88af2910dac6d940a646e6ccdc9f73b66de7a300
GANG철\kang6 <kmc6381@gmail.com>
2023-05-02 15:36:02 +0900

    [문제3] 문자형 변수 유니코드로 변환 문제
    문제를 풀었습니당.

---
+++ src/main/java/com/ohgiraffers/level01/basic/Apple.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.level01.basic;
+
+public class Apple {
+    public static void main(String[] args) {
+
+
+
+        int x = 1;
+        int y = 2;
+
+        System.out.println("x + y = " + (x + y));
+    }
+}
+
