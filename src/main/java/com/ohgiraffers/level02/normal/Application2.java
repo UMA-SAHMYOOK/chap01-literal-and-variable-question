7c7a1f55982f0313e04c385a13541f677e2bb963
CHOI <seongyong335@gmail.com>
2023-05-02 13:34:36 +0900

    [normal][문제2] 완료
    국어, 수학, 영어의 점수가 실수 형태로 값을 가지고 저장이 되어야하기에 실수형 데이터타입 double에 각각 해당하는 값을 선언하였습니다.
    
    모든 출력문이 문자열과의 결합형식이라 괄호를 이용하여 문자열과 사칙연산을 결합했습니다.
    총점은 각 과목의 총 합이기에 모든 값을 더하고 정수형으로 출력하기 위해 int로 강제 형변환 후 출력하였습니다.
    평균은 총점에서 과목의 갯수만큼 나눈 값이기에 총점을 구한 식에다가 과목의 갯수 3을 나누었습니다.

--- src/main/java/com/ohgiraffers/level02/normal/Application2.java
+++ src/main/java/com/ohgiraffers/level02/normal/Application2.java
@@ -3,7 +3,7 @@ package com.ohgiraffers.level02.normal;
 public class Application2 {
 
 	public static void main(String[] args) {
-		
+
 		/* 국어점수 80.5점, 수학점수 50.6점, 영어점수 70.8점을 실수 형태로 저장한 뒤
 		 * 총점과 평균을 정수 형태로 출력하세요
 		 * 
@@ -11,7 +11,14 @@ public class Application2 {
 		 * 총점 : 201
 		 * 평균 : 67
 		 *  */
-		
+		double Korean = 80.5;
+		double math = 50.6;
+		double English = 70.8;
+
+		System.out.println("---- 출력 ----");
+		System.out.println("총점 : " + (int)(Korean + math + English));
+		System.out.println("평균 : " + ((int)(Korean + math + English) / 3));
+		System.out.println("-------------");
 	}
 
 }
